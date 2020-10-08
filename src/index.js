cab274c55903d74cf5e09c503d8503aa80a2e53a
Claire Moore <mooreclaire95@gmail.com>
2020-10-08 15:40:22 -0400

    Provider and reducer setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 //connect our app to our root folder
-
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {Provider} from 'react-redux'
+import store from './store'
 import App from "./App";
-
+import {Router} from 'react-router-dom'
+import history from './history'
 
 //so both broadcaster and watcher can not be on at the same time :(
 //i am thinking that this is only really one person prodcasting out to others.
@@ -13,4 +14,11 @@ import './socket'
 import './broadcaster'
 // import './watcher'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>    
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById("root")
+);
