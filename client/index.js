ac842241abcf2eaa9e7d168c00bf0c40d60f48e9
Claire Moore <mooreclaire95@gmail.com>
2020-10-08 16:01:34 -0400

    provider and reducer

--- client/index.js
+++ client/index.js
@@ -2,10 +2,20 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {Provider} from 'react-redux'
+import store from './store'
+import {Router} from 'react-router-dom'
+import history from './history'
 import App from "./App";
 
 import './socket'
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>    
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById("root")
+);
