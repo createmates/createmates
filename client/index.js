9da5b1ac4cda3461db0b593a419e165f052226a4
Kelsey Schroeder <kelsey.schroeder@att.net>
2020-10-08 14:54:00 -0500

    revamp

--- client/index.js
+++ client/index.js
@@ -5,12 +5,7 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-
-//so both broadcaster and watcher can not be on at the same time :(
-//i am thinking that this is only really one person prodcasting out to others.
-
 import './socket'
-import './broadcaster'
-// import './watcher'
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,32 +1,14 @@
-let  broadcaster
-
-module.exports = (io) => {
-  io.on("connection", (socket) => {
-    console.log(
-      `A socket connection to the server has been made: ${socket.id}`
-    );
-    socket.on("disconnect", () => {
-      console.log(`Connection ${socket.id} has left the building`);
-    });
-    socket.on("broadcaster", () => {
-    broadcaster = socket.id
-    socket.broadcast.emit("broadcaster")
-    });
-    socket.on("watcher", () => {
-        socket.to(broadcaster).emit("watcher", socket.id);
-    });
-    socket.on("disconnect", () => {
-        console.log('sockect has left the building')
-        socket.to(broadcaster).emit("disconnectPeer", socket.id)
-    })
-    socket.on("offer", (id, message) => {
-        socket.to(id).emit("offer", socket.id, message);
-    });
-    socket.on("answer", (id, message) => {
-      socket.to(id).emit("answer", socket.id, message);
-    });
-    socket.on("candidate", (id, message) => {
-      socket.to(id).emit("candidate", socket.id, message);
-    });
-  });
-};
+module.exports = io => {
+  io.on('connection', socket => {
+    console.log(`A socket connection to the server has been made: ${socket.id}`)
+      socket.on('join-room', (roomId, userId) => {
+          console.log('roomid',roomId, 'userId',userId)
+          socket.join(roomId)
+          socket.to(roomId).broadcast.emit("user-connected", userId)
+          socket.on('disconnect', () => {
+            console.log(`Connection ${socket.id} has left the building`)
+            socket.to(roomId).broadcast.emit('user-disconnected',  userId)
+          })
+      })
+  })
+}
