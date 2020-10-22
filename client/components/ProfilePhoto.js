6c9c4ccc075c8bc85faef28367f6db7c871142fc
Claire Moore <mooreclaire95@gmail.com>
2020-10-22 15:41:57 -0400

    maybe posting to digital works

--- client/components/ProfilePhoto.js
+++ client/components/ProfilePhoto.js
@@ -1,38 +1,70 @@
 import React from 'react'
 import axios from 'axios'
-// import {connect} from 'react-redux'
+import {updateUserThunk} from '../store/user'
+import {connect} from 'react-redux'
+const aws = require('aws-sdk');
+if (process.env.NODE_ENV === "development") require('../../secrets');
+
+const spacesEndpoint = new aws.Endpoint('nyc3.digitaloceanspaces.com');
+const s3 = new aws.S3({
+    endpoint: spacesEndpoint,
+    accessKeyId: process.env.DIGITAL_OCEAN_SPACES_API_ACCESS_KEY,
+    secretAccessKey: process.env.DIGITAL_OCEAN_SPACES_SECRET,
+    region: 'nyc3'
+});
 
 class ProfilePhoto extends React.Component {
   constructor() {
     super()
     this.state = {
-      profilePic: {}
+      fileName: ''
     }
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-handleSubmit = async (event) => {
-  event.preventDefault();
-  try {
-    console.log("Event.target: ", event.target)
-    const pic = event.target.value
-    console.log("Pic: ", pic)
-    await axios.post(`/spaces/upload`, pic)
-  } catch (err) {
-    console.error(err)
+  handleSubmit = async (event) => {
+    event.preventDefault()
+    const selectedFile = document.getElementById('file').files[0]
+    console.log("selected file: ", selectedFile)
+    console.log("selected file.name: ", selectedFile.name)
+    try {
+      const params = {
+        Bucket: "createmates",
+        Key: selectedFile.name,
+        Body: selectedFile,
+        ACL: 'public-read'
+      };
+      await s3.putObject(params, function(err, data) {
+        if (err) console.log(err, err.stack);
+        else     console.log(data);
+      });
+      // this.props.updateUser(this.props.user.id, {profilePhoto: `https://createmates.nyc3.digitaloceanspaces.com/${selectedFile.name}`})
+    } catch (err) {
+      console.error(err)
+    }
   }
-}
 
 render () {
   return (
-    this.state.profilePic.name ? <p>We have a pic</p> : 
-    <form method="post" encType="multipart/form-data" onSubmit={this.handleSubmit} action="/spaces/upload" >
+    <form method="post" encType="multipart/form-data" onSubmit={this.handleSubmit}>
       <label htmlFor="file">Upload file</label>
-      <input type="file" name="upload" />
+      <input id="file" type="file" name="upload" />
       <button type="submit">Submit</button>
     </form>
     )
   }
 }
 
-export default ProfilePhoto;
+const mapState = state => {
+  return {
+    user: state.profile
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    updateUser: (userId, obj) => dispatch(userId, updateUserThunk())
+  }
+}
+
+export default connect(mapState, mapDispatch)(ProfilePhoto);
--- server/digitalOcean/spaces.js
+++ server/digitalOcean/spaces.js
@@ -1,39 +1,49 @@
 const aws = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
+// const multer = require('multer');
+// const multerS3 = require('multer-s3');
 const router = require("express").Router();
+const fs = require('fs');
 module.exports = router;
 
-
-aws.config.update({
-  secretAccessKey: process.env.DIGITAL_OCEAN_SPACES_SECRET,
-  accessKeyId: process.env.DIGITAL_OCEAN_SPACES_API_ACCESS_KEY,
-  region: 'nyc3'
-});
-
 const spacesEndpoint = new aws.Endpoint('nyc3.digitaloceanspaces.com');
-
 const s3 = new aws.S3({
-  endpoint: spacesEndpoint
+    endpoint: spacesEndpoint,
+    accessKeyId: process.env.DIGITAL_OCEAN_SPACES_API_ACCESS_KEY,
+    secretAccessKey: process.env.DIGITAL_OCEAN_SPACES_SECRET,
+    region: 'nyc3'
 });
 
-const upload = multer({
-    storage: multerS3({
-      s3: s3,
-      bucket: 'createmates',
-      acl: 'public-read',
-      key: function (request, file, cb) {
-        console.log(file);
-        cb(null, file.originalname);
-      }
-    })
-  }).array('upload', 1);
+// const upload = multer({
+//     storage: multerS3({
+//       s3: s3,
+//       bucket: 'createmates',
+//       acl: 'public-read',
+//       key: function (request, file, cb) {
+//         console.log(file);
+//         cb(null, file.originalname);
+//       }
+//     })
+//   }).array('upload', 1);
 
-  router.post('/upload', function (request, response, next) {
-    upload(request, response, function (error) {
-      if (error) {
-        console.log(error);
-      } else {
-      console.log('File uploaded successfully.');
-    }});
+  // router.post('/upload', function (request, response, next) {
+  //   upload(request, response, function (error) {
+  //     if (error) {
+  //       console.log(error);
+  //     } else {
+  //     console.log('File uploaded successfully.');
+  //   }});
+  // });
+
+var params = {
+    Bucket: "createmates",
+    Key: "file.ext",
+    Body: "The contents of the file.",
+    ACL: "public-read"
+};
+
+router.post('/upload', (req, res, next) => {
+    s3.putObject(params, function(err, data) {
+      if (err) console.log(err, err.stack);
+      else     console.log(data);
   });
+})
