321b4568672f06503e2660b798267d820487b47a
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-03-24 15:36:20 -0500

    Fix: bug with the ssl line of the db file from boilerplate

--- server/db/db.js
+++ server/db/db.js
@@ -1,19 +1,25 @@
-const Sequelize = require("sequelize");
-const pkg = require("../../package.json");
-
-const databaseName =
-  pkg.name + (process.env.NODE_ENV === "test" ? "_test" : "");
-
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  {
+const Sequelize = require('sequelize')
+const pkg = require('../../package.json')
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+let db
+if (process.env.DATABASE_URL) {
+  // heroku configuration
+  db = new Sequelize(process.env.DATABASE_URL, {
     logging: false,
-  }
-);
-module.exports = db;
-
-// This is a global Mocha hook used for resource cleanup.
-// Otherwise, Mocha v4+ does not exit after tests.
-if (process.env.NODE_ENV === "test") {
-  after("close database connection", () => db.close());
+    dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: true
+    }
+  })
+} else {
+  // local configuration
+  db = new Sequelize(`postgres://localhost:5432/${databaseName}`, {
+    logging: false
+  })
+}
+module.exports = db
+if (process.env.NODE_ENV === 'test') {
+  after('close database connection', () => db.close())
 }
